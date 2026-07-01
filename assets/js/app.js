3f514f912269eb0e6bea08214939ee67091af4d5
Escalante Hugo <palofito40@gmail.com>
2026-06-30 22:18:11 -0300

    Se ha añadido buscador en el app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,7 +5,6 @@ document.addEventListener("DOMContentLoaded",async (event) => {
 
    const personajesMostrados = personajesCard.results
 
-   console.log(personajesMostrados)
 
    const personajesElement = document.querySelector("#cardPersonaje")
 
@@ -28,4 +27,42 @@ document.addEventListener("DOMContentLoaded",async (event) => {
           </div>`       
     });
 
+   const busquedaPersonaje = document.querySelector("#busquedaPersonaje")
+
+   busquedaPersonaje.addEventListener('input', (event)=>{
+    
+    const textUser = event.target.value.toLowerCase()
+
+
+    const personasjeResultados = personajesMostrados.filter( (cardPersonaje)=>{
+        return cardPersonaje.name.toLowerCase().includes(textUser)
+    })
+    console.log(textUser)
+
+    personajesElement.innerHTML = "";
+
+    if(personajesElement.lenght===0){
+        `<div class="col-12 text-center mt-5">
+            <h3 class="text-muted">No se encontraron personajes para tu búsqueda </h3>
+        </div>`;
+    } else{personasjeResultados.forEach(cardPersonaje=>{
+        personajesElement.innerHTML +=
+        ` <div class="col-12 col-md-4">
+            <div class="card" style="width: 18rem">
+              <img src="https://cdn.thesimpsonsapi.com/500${cardPersonaje.portrait_path}" class="card-img-top" alt="${cardPersonaje.name}"/>
+              <div class="card-body">
+                <h5 class="card-title">${cardPersonaje.name}</h5>
+                <p class="card-text">
+                  ${cardPersonaje.occupation}
+                </p>
+                <p class="card-text">
+                  ${cardPersonaje.status}
+                </p>
+                <a href="#" class="btn btn-primary">Ver Detalles</a>
+              </div>
+            </div>
+          </div>` })
+    }
+   }) 
+  
 })
